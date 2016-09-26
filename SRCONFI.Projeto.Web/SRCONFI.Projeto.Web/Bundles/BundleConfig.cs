bf460507e17830e71dc1d0aafb660150650b2fd6
Jaderson Elias <jaderson.elias@gmail.com>
2016-09-25 21:39:49 -0300

    alterado o indext

---
+++ SRCONFI.Projeto.Web/SRCONFI.Projeto.Web/Bundles/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+
+namespace SRCONFI.Projeto.Web.Bundles
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+        }
+    }
+}

df10a9e1b78436544bd5dce1863d4049de061726
Jaderson Elias <jaderson.elias@gmail.com>
2016-12-13 19:32:58 -0200

    Commit Doações

---
+++ SRCONFI.Projeto.Business/DoacoesBusiness.cs
@@ -0,0 +1,60 @@
+﻿using SRCONFI.Projeto.Domain.UnitOfWork;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SRCONFI.Projeto.Business
+{
+    public class DoacoesBusiness
+    {
+        public List<Domain.Entity.Doacoes> ListDoacoes()
+        {
+            List<Domain.Entity.Doacoes> listDoacoes = new List<Domain.Entity.Doacoes>();
+            using (var unitOfWork = new UnitOfWork(new Domain.BancoContext()))
+            {
+                listDoacoes = unitOfWork.Doacoes.GetAll().ToList();
+            }
+            return listDoacoes;
+        }
+
+        public void AddDoacoes(Domain.Entity.Doacoes doacoes)
+        {
+            using (var unitOfWork = new UnitOfWork(new Domain.BancoContext()))
+            {
+                unitOfWork.Doacoes.Add(doacoes);
+                unitOfWork.Complete();
+                unitOfWork.Dispose();
+            }
+        }
+        public void EditDoacoes(Domain.Entity.Doacoes doacoes)
+        {
+            using (var unitOfWork = new UnitOfWork(new Domain.BancoContext()))
+            {
+                unitOfWork.Doacoes.Edit(doacoes);
+                unitOfWork.Complete();
+                unitOfWork.Dispose();
+            }
+        }
+        public void DeleteDoacoes(int id)
+        {
+            using (var unitOfWork = new UnitOfWork(new Domain.BancoContext()))
+            {
+                var doacoes = unitOfWork.Doacoes.Get(id);
+                unitOfWork.Doacoes.Remove(doacoes);
+                unitOfWork.Complete();
+                unitOfWork.Dispose();
+            }
+        }
+        public Domain.Entity.Doacoes GetDoacoes(int idDoacoes)
+        {
+            Domain.Entity.Doacoes doacoes;
+            using (var unitOfWork = new UnitOfWork(new Domain.BancoContext()))
+            {
+                doacoes = unitOfWork.Doacoes.Get(idDoacoes);
+                unitOfWork.Dispose();
+            }
+
+            return doacoes;
+        }
+
+    }
+}
---
+++ SRCONFI.Projeto.Web/SRCONFI.Projeto.Web/scripts/Views/Doacoes/Inserir.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    $("#btnIncluirDoacoes").click(function () {
+        inserirDoacoes();
+    });
+
+    maskFormat();
+
+    $("#Endereco_numero").formatter({
+        'pattern': '{{9999}}',
+        'persistent': true
+    });
+});
+
+function abrirModalEditar(id) {
+    var urlModal = $("#hdnCaminhoModalEditarDoacoes").val();
+    var urlEditar = $("#hdnCaminhoEditarDoacoes").val() + '?id=' + id;
+
+    abrirModal(urlModal, urlEditar);
+}
+
+function inserirDoacoes() {
+    var validForm = $('#formInserirDoacoes').parsley();
+    var form = $('#formInserirDoacoes').serializeObject();
+
+    if (validForm.validate()) {
+        $.ajax({
+            url: $('#hdnCaminhoInserirDoacoes').val(),
+            type: "POST",
+            data: JSON.stringify({ doacoes: form }),
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                if (!data.erro) {
+                    atualizarTableDoacoes();
+                    abrirModalEditar(data.id);
+                    alertSistema(1, data.mensagem);
+                }
+                else {
+                    alertSistema(2, data.mensagem);
+                }
+
+            },
+            error: function (response) {
+                var r = jQuery.parseJSON(response.responseText);
+                alert("Message: " + r.Message);
+                alert("StackTrace: " + r.StackTrace);
+                alert("ExceptionType: " + r.ExceptionType);
+            }
+        });
+    }
+}

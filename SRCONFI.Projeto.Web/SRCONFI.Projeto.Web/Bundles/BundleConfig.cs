33c738f2f663208f5b839bb7f54e8d91bd86d4d8
Gabriel Borba de Oliveira <gabriel.boliveira@yahoo.com.br>
2017-05-01 20:46:38 -0300

    Bundles e layout.

--- SRCONFI.Projeto.Web/SRCONFI.Projeto.Web/Bundles/BundleConfig.cs
+++ SRCONFI.Projeto.Web/SRCONFI.Projeto.Web/Bundles/BundleConfig.cs
@@ -6,23 +6,31 @@ namespace SRCONFI.Projeto.Web.Bundles
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/scripts/required").Include(
+                        "~/Scripts/jquery-1.12.4.min.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/jquery-ui-1.12.1.min.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Content/jquery-ui.css",
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(                        
                         "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/bootstrap.mim.css",
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(                      
                       "~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/parsley").Include(
-                  "~/Content/parsley.css",
+            bundles.Add(new ScriptBundle("~/bundles/parsley").Include(                  
                   "~/Scripts/parsley-2.6.0.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerymask").Include(
-                "~/Scripts/jquery.mask-1.14.3.js"));
+            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
+             "~/Scripts/jquery.inputmask/inputmask.js",
+             "~/Scripts/jquery.inputmask/jquery.inputmask.js",
+             "~/Scripts/jquery.inputmask/inputmask.extensions.js",
+             "~/Scripts/jquery.inputmask/inputmask.date.extensions.js",
+             //and other extensions you want to include
+             "~/Scripts/jquery.inputmask/inputmask.numeric.extensions.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
@@ -31,8 +39,13 @@ namespace SRCONFI.Projeto.Web.Bundles
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content").Include(
-              "~/Content/jquery.validate.bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css/required").Include(
+              "~/Content/bootstrap.min.css",
+              "~/Content/themes/base/jquery-ui.min.css"));
+            //,
+            //  "~/Content/jquery.validate.bootstrap.css"
+
+            bundles.Add(new StyleBundle("~/Content/css/parsley").Include("~/Content/Css/Parsley/parsley.css"));
 
             bundles.Add(new StyleBundle("~/Content/css/Layout").Include("~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css/Layout/index").Include("~/Content/Css/Layout/index.css"));
